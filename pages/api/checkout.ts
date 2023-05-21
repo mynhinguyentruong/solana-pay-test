85503d0ad73215128afc1ba1078aa8f7d802fa09
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-05-21 11:52:48 -0400

    Create Base64 serialized transaction to send to wallet

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -1,11 +1,12 @@
-import {NextApiHandler, NextApiRequest, NextApiResponse} from "next";
+import { NextApiRequest, NextApiResponse} from "next";
 import {clusterApiUrl, Connection, Keypair, PublicKey, Transaction} from "@solana/web3.js";
 import {getAccount, getAssociatedTokenAddress, getMint,createTransferCheckedInstruction} from "@solana/spl-token";
-import {createTransfer, createQR, encodeURL} from "@solana/pay";
-//import {connection} from "../core
+// We don't need this because we're creating transaction
+// import {createTransfer, createQR, encodeURL} from "@solana/pay";
+import BigNumber from "bignumber.js";
 
-const MERCHANT_WALLET = new PublicKey(JSON.parse(process.env.MERCHANT_PRIVATE_KEY)) // probably wrong
-const splToken = new PublicKey('4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU')
+const MERCHANT_WALLET = Keypair.fromSecretKey(Uint8Array.from(JSON.parse(process.env.MERCHANT_PRIVATE_KEY))).publicKey
+const splToken = new PublicKey('Hsybc16M3SvBkFdAUSYMVT3q2u2NLT8odnutZWjWULTt')
 
 
 type GetResponse = {
@@ -19,14 +20,19 @@ type PostResponse = {
 }
 
 export default async function apiHandler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'get') {
+    if (req.method === 'GET') {
         get(res)
+    }
+
+    if (req.method === 'POST') {
+        await post(req, res)
     }
 }
 
 function get(res: NextApiResponse<GetResponse>) {
+
     return res.status(200).json({
-        label: "My Store",
+        label: "Nhi IS DUMB Store",
         icon: "https://s2.coinmarketcap.com/static/img/coins/200x200/5426.png"
     })
 }
@@ -39,7 +45,6 @@ async function post(req, res) {
     // why do we need keypair here?
     // properly to verify that the store is the one who is sending transaction
    // do something here
-    const shopKeypair = Keypair.fromSecretKey(JSON.parse(process.env.PRIVATE_KEY))
 
     // create spl token associate with shopkeypair
 
@@ -52,11 +57,11 @@ async function post(req, res) {
 
     // Get the shop's USDC address
 
-    const recipientField = req.query.recipient;
-    if (!recipientField) throw new Error("missing recipient")
-    if (typeof recipientField !== 'string') throw new Error('invalid recipient');
-
-    const recipient_pubkey = new PublicKey(recipientField);
+    // const recipientField = req.query.recipient;
+    // if (!recipientField) throw new Error("missing recipient")
+    // if (typeof recipientField !== 'string') throw new Error('invalid recipient');
+    //
+    // const recipient_pubkey = new PublicKey(recipientField);
 
     const amountField = req.query.amount;
     // POST <url>/something?amount=1&recipient=jfssdfj434SolanaAddress
@@ -74,17 +79,46 @@ async function post(req, res) {
 
     const sender = new PublicKey(accountField)
 
-    const splTransferIx = await createSplTokenTransferIx(sender, connection)
+    // let some_transaction = createTransfer(connection, new PublicKey(accountField), {
+    //     recipient,
+    //     amount,
+    //     splToken,
+    //     reference,
+    //     memo
+    // })
+    console.log({sender})
 
-    const transaction = new Transaction().add(splTransferIx)
+    let splTransferIx
+    try {
+         splTransferIx =  await createSplTokenTransferIx(sender, connection)
+    } catch (e) {
+        console.log(e)
+    }
+
+    let transaction = new Transaction().add(splTransferIx);
+
+    let blockhash = (await connection.getLatestBlockhash()).blockhash
+
+    transaction.recentBlockhash = blockhash;
+    transaction.feePayer = new PublicKey(0)
+
+    // transaction = Transaction.from(
+    //     transaction.serialize({
+    //         verifySignatures: false,
+    //         requireAllSignatures: false,
+    //     })
+    // );
+
 
     const serializedTransaction = transaction.serialize({
         verifySignatures: false,
-        requireAllSignatures: false
+        requireAllSignatures: false,
     })
 
     const base64Transaction = serializedTransaction.toString('base64');
     const message = 'Thank you for your purchase of ExiledApe #518';
+
+    console.log({base64Transaction})
 
     res.status(200).send({ transaction: base64Transaction, message})
 }
@@ -92,13 +126,16 @@ async function post(req, res) {
 
 // getOrCreateAssociateTokenAccount(connection, shopKeypair, USDC_ADDRESS, shopKeypair.publicKey)
 // create SplTransferIx(sender, connection) -> create spl transfer instruction
-async function createSplTokenTransferIx(sender, connection) {
+async function createSplTokenTransferIx(sender: PublicKey, connection: Connection) {
+
 
     const senderInfo = await connection.getAccountInfo(sender);
+
     if (!senderInfo) throw new Error('sender not found');
 
     // Get the sender's ATA and check that the account exists and can send tokens
     const senderATA = await getAssociatedTokenAddress(splToken, sender);
+    if (!senderATA) throw new Error("Cannot get sebderATA")
     const senderAccount = await getAccount(connection, senderATA);
     if (!senderAccount.isInitialized) throw new Error('sender not initialized');
     if (senderAccount.isFrozen) throw new Error('sender frozen');
@@ -111,6 +148,8 @@ async function createSplTokenTransferIx(sender, connection) {
 
     // Check that the token provided is an initialized mint
     const mint = await getMint(connection, splToken);
+
+    console.log({mint})
     if (!mint.isInitialized) throw new Error('mint not initialized');
 
     // You should always calculate the order total on the server to prevent
@@ -119,9 +158,10 @@ async function createSplTokenTransferIx(sender, connection) {
     // amount = amount.times(TEN.pow(mint.decimals)).integerValue(BigNumber.ROUND_FLOOR);
 
     // Check that the sender has enough tokens
-    const tokens = BigInt(String(1));
+    // let amount = BigNumber(10).times(Math.pow(mint.decimals)).integerValue(BigNumber.ROUND_FLOOR);
+    let amount = BigNumber(10).times(Math.pow(10, mint.decimals) ).integerValue(BigNumber.ROUND_FLOOR)
+    const tokens = BigInt(String(amount));
     if (tokens > senderAccount.amount) throw new Error('insufficient funds');
-
     // Create an instruction to transfer SPL tokens, asserting the mint and decimals match
     const splTransferIx = createTransferCheckedInstruction(
         senderATA, // source
@@ -134,11 +174,14 @@ async function createSplTokenTransferIx(sender, connection) {
 
     // Create a reference that is unique to each checkout session
     const references = [new Keypair().publicKey];
+    console.log({references})
 
     // add references to the instruction
     for (const pubkey of references) {
         splTransferIx.keys.push({ pubkey, isWritable: false, isSigner: false });
     }
+
+    console.log({splTransferIx})
 
     return splTransferIx;
 }
