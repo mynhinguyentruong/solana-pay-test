f9a28ec4442237bbae673e2d27a3b33ea00db9e9
Nhi Nguyen <64499617+mynhinguyentruong@users.noreply.github.com>
2023-07-22 11:04:13 -0400

    remove unnecessary arg

--- pages/api/qrcode.ts
+++ pages/api/qrcode.ts
@@ -1,30 +1,40 @@
-import {Keypair, PublicKey} from "@solana/web3.js";
+import { Keypair, PublicKey } from "@solana/web3.js";
 import BigNumber from "bignumber.js";
-import {createQR, encodeURL} from "@solana/pay";
+import { createQR, encodeURL } from "@solana/pay";
 
 export default async function apiHandler(req, res) {
-    console.log(JSON.parse(process.env.MERCHANT_PRIVATE_KEY))
-    const keypair = Keypair.fromSecretKey(Uint8Array.from(JSON.parse(process.env.MERCHANT_PRIVATE_KEY)))
-    const recipient = keypair.publicKey
-    console.log({recipient})
-    const amount = new BigNumber(0.001);
-    const originalReference = Keypair.generate().publicKey;
-    const label = 'Nhi is dumb store';
-    const message = 'Nhi is dumb store - your order - #001234';
-    const memo = 'JC#4098';
-    const link = new URL('https://solana-send-sol-frontend-nhi-ymihn.vercel.app/api/checkout')
+  console.log(JSON.parse(process.env.MERCHANT_PRIVATE_KEY));
+  const keypair = Keypair.fromSecretKey(
+    Uint8Array.from(JSON.parse(process.env.MERCHANT_PRIVATE_KEY))
+  );
+  const recipient = keypair.publicKey;
+  console.log({ recipient });
+  const amount = new BigNumber(0.001);
+  const originalReference = Keypair.generate().publicKey;
+  const label = "Nhi is dumb store";
+  const message = "Nhi is dumb store - your order - #001234";
+  const memo = "JC#4098";
+  const link = new URL(
+    "https://solana-send-sol-frontend-nhi-ymihn.vercel.app/api/checkout"
+  );
 
-    const testUrl = encodeURL({link, label, message})
+  const testUrl = encodeURL({ link });
 
-    const url = encodeURL({ recipient, amount, reference: originalReference, label, message, memo });
-    console.log({url})
-    // const qrCode = createQR(url);
-    // console.log(qrCode)
-    // console.log(qrCode)
-    // console.log(qrCode)
+  const url = encodeURL({
+    recipient,
+    amount,
+    reference: originalReference,
+    label,
+    message,
+    memo,
+  });
+  console.log({ url });
+  // const qrCode = createQR(url);
+  // console.log(qrCode)
+  // console.log(qrCode)
+  // console.log(qrCode)
 
-    // res.status(200).send(JSON.stringify(url))
-    // {url: 'solana:9TQ1czXPRY1zeQR2cYeVVg4vmKJPXQwX6GGdkuf7NU5…ats+store+-+your+order+-+%23001234&memo=JC%234098'}
-    res.status(200).send({url, testUrl})
-
+  // res.status(200).send(JSON.stringify(url))
+  // {url: 'solana:9TQ1czXPRY1zeQR2cYeVVg4vmKJPXQwX6GGdkuf7NU5…ats+store+-+your+order+-+%23001234&memo=JC%234098'}
+  res.status(200).send({ url, testUrl });
 }
